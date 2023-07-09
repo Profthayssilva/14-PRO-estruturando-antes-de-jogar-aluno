2c3d5f94348bb539f4cb75fe20c1ceeec526a442
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2023-07-09 18:52:31 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,13 @@
+class Game {
+  constructor() {}
+
+  start() {
+
+//     Crie um objeto Form dentro da classe
+// de jogo chamada game.
+
+    // Chame a função display().
+
+    player = new Player();
+  }
+}
